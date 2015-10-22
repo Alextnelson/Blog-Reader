2d6636c240d4c04a148cfb36a4c2795bee9b9395
Alextnelson <alexander.thomas.nelson@gmail.com>
2015-10-22 14:07:27 -0500

    Initial Commit

---
+++ Blog Reader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blog Reader
+//
+//  Created by Alexander Nelson on 10/22/15.
+//  Copyright © 2015 Jetwolfe Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
